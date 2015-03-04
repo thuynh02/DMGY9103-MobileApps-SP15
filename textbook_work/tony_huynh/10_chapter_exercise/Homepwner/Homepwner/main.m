bbd6daf30a124513b6c9f856bab65249731a765c
Tony H <th1142@nyu.edu>
2015-03-04 00:42:26 -0500

    ch10 exercises done

---
+++ textbook_work/tony_huynh/10_chapter_exercise/Homepwner/Homepwner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Tony H on 2/22/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
