a23b08dfe9be7304dbf289d705a468ac8bd7a33c
Tony H <th1142@nyu.edu>
2015-03-21 17:13:44 -0400

    finished ch 13 exercises

---
+++ textbook_work/tony_huynh/13_chapter_exercise/TouchTracker/TouchTracker/BNRLine.h
@@ -0,0 +1,17 @@
+//
+//  BNRLine.h
+//  TouchTracker
+//
+//  Created by Tony H on 3/10/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface BNRLine : UIView
+
+@property (nonatomic) CGPoint begin;
+@property (nonatomic) CGPoint end;
+
+@end
